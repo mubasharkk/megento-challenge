96cf2b09ef79be10720cd6d1fb6ef096413fc234
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2016-06-04 22:32:12 +0200

    Module block class added

---
+++ Challenge/Block/Product/View/Suggest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BettenReise\Challenge\Block\Product\View;
+
+use \Magento\Framework\View\Element\Template;
+
+/**
+ * Description of Suggest
+ *
+ * @author mubasharkk
+ */
+
+class Suggest extends Template {
+  
+    public function getTitle(){
+	  
+        return "Foo Bar Baz";
+    }
+  
+}
